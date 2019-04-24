cea0076c60827adf101f31d48e3303bacfe404ca
Humberto Kuruklis <hakuruklis@gmail.com>
2019-04-24 14:46:46 -0500

    views de webempresa

--- Curso Django/webempresa/core/views.py
+++ Curso Django/webempresa/core/views.py
@@ -8,9 +8,6 @@ def home(request):
 def about(request):
     return render(request,"core/about.html")
 
-def services(request):
-    return render(request,"core/services.html")
-
 def store(request):
     return render(request,"core/store.html")
 
@@ -18,7 +15,7 @@ def contact(request):
     return render(request,"core/contact.html")
 
 def blog(request):
-    return render(request,"core/bloghome.html")
+    return render(request,"core/blog.html")
 
 def sample(request):
     return render(request,"core/sample.html")
--- Curso Django/webempresa/webempresa/urls.py
+++ Curso Django/webempresa/webempresa/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     #Paths Core
     path('', include('core.urls')),
+    #Paths de Services
+    path('services/', include('services.urls')),
     #Paths Admin
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
