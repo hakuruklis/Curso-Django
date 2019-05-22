ff520774bdce4b67abba89921ef08ada000cdfc3
Humberto Kuruklis <hakuruklis@gmail.com>
2019-05-21 20:30:51 -0500

    terminando web empresa

--- Curso Django/webempresa/pages/models.py
+++ Curso Django/webempresa/pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Titulo", max_length=200)
     content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
 
     class Meta:
         verbose_name = "página"
         verbose_name_plural = "páginas"
-        ordering = ['title']
+        ordering = ['order','title']
 
     def __str__(self):
         return self.title
