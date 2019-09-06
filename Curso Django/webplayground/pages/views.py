1fd3eb23ce043026ddab1a303c225cb8aa2da30e
Humberto Kuruklis <hakuruklis@gmail.com>
2019-09-06 16:22:47 -0500

    no message

--- Curso Django/webplayground/pages/views.py
+++ Curso Django/webplayground/pages/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 from .models import Page
 
-# Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+# Create your views here.
+class PageListView(ListView):
+    model = Page
+
+class PageDetailView(DetailView):
+    model = Page
+
+class PageCreate(CreateView):
+    model= Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    template_name_suffix = '_update_form'
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
